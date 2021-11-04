b0bd2ad5e7f7769aada03ede1710d03d2c31ac89
Antoine BIGEARD <antoine.bigeard.tpe@gmail.com>
2021-11-04 09:26:45 +0100

    Ajoutez des fichiers projet.

---
+++ Veille/Screen/AsposeScreen.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Veille.Tools;
+
+namespace Veille.Screen
+{
+    /// <summary>
+    /// Logique d'interaction pour AsposeScreen.xaml
+    /// </summary>
+    /// 
+    
+    public partial class AsposeScreen : Window
+    {
+
+        public AsposeScreen()
+        {
+            InitializeComponent();
+        }
+
+        private void OpenFile(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void WriteFile(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Veille/Tools/Timer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Veille.Tools
+{
+    public class Timer
+    {
+        private static Stopwatch clock { get; set; } = new Stopwatch();
+
+        public static void Start()
+        {
+            clock.Reset();
+            clock.Start();
+        }
+
+        public static void Stop()
+        {
+            clock.Stop();
+        }
+
+        public static string GetTime()
+        {
+            return clock.ElapsedMilliseconds.ToString() + " ms";
+        }
+    }
+}
